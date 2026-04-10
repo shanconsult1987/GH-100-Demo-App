ed4a76d2de90ff41619ee886b4e1e60dedf2a3ec
shanconsult1987 <78853500+shanconsult1987@users.noreply.github.com>
2026-04-10 14:56:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    user_input = request.args.get("input")
+    eval(user_input)  # ❌ Intentional vulnerability
     return "Hello GH-100 Admin Demo 🚀"
-
-if __name__ == "__main__":
-    app.run(debug=True)
